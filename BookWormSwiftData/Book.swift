879de43c5b74a04b253be2e1c485b9164a134647
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-10-16 01:04:56 +0300

    add model

--- BookWormSwiftData/Book.swift
+++ BookWormSwiftData/Book.swift
@@ -5,4 +5,22 @@
 //  Created by Marat Fakhrizhanov on 15.10.2024.
 //
 
-import Foundation
+import SwiftData
+import SwiftUI
+
+@Model
+class Book {
+    var title: String
+    var author: String
+    var genre: String
+    var review: String
+    var rating: Int
+    
+    init(title: String, author: String, genre: String, review: String, rating: Int) {
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.review = review
+        self.rating = rating
+    }
+}
