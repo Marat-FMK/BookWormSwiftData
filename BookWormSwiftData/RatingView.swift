9730ce3d3b013992c49cdf7c0adfb7669b2f50f6
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-10-16 01:06:43 +0300

    add view for rating stars

--- BookWormSwiftData/RatingView.swift
+++ BookWormSwiftData/RatingView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct RatingView: View {
+    
+    @Binding var rating: Int
+    
+    var label = ""
+    
+    var maximumRating = 5
+    
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+    
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            if label.isEmpty == false {
+                Text(label)
+            }
+
+            ForEach(1..<maximumRating + 1, id: \.self) { number in
+                Button {
+                    rating = number
+                } label: {
+                    image(for: number)
+                        .foregroundStyle(number > rating ? offColor : onColor)
+                }
+            }
+        }
+        .buttonStyle(.plain) // Что бы Свифт обрабатывал каждую кнопку индивидуально// прописываем что бы при нажатии на экране АДД БУК не ставилось сразу 5 звезд , тк на том экране СВИФТ ЮИ будет наживать на каждую кнопку нашего представления поочереди и в итоге остаовится на 5 звездах урок pr 11? 5/10
     }
+    
+    func image(for number: Int) -> Image {
+        if number > rating {
+            offImage ?? onImage
+        } else {
+            onImage
+        }
+    }
+    
 }
 
 #Preview {
-    RatingView()
+    RatingView(rating: .constant(4))
 }
