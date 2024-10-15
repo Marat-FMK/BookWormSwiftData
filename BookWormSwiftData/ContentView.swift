a197220eeab2ee55d31eb5907adadb07e4b7ab64
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-10-16 01:05:45 +0300

    work with logic

--- BookWormSwiftData/ContentView.swift
+++ BookWormSwiftData/ContentView.swift
@@ -5,17 +5,61 @@
 //  Created by Marat Fakhrizhanov on 15.10.2024.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.modelContext) var modelContext
+    @Query var books: [Book]
+    
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(books) { book in
+                    NavigationLink(value: book) {
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+
+                            VStack(alignment: .leading) {
+                                Text(book.title)
+                                    .font(.headline)
+                                Text(book.author)
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                    }
+                }
+                .onDelete(perform: deleteBooks) // именнно на фор Ич ставим делит
+            }
+               .navigationTitle("Bookworm")
+               .navigationDestination(for: Book.self, destination: { book in
+                   DetailView(book: book)
+               })
+               .toolbar {
+                   ToolbarItem(placement: .topBarLeading) {
+                       EditButton() // Едит должен стоять левее
+                   }
+                   
+                   ToolbarItem(placement: .topBarTrailing) {
+                       Button("Add Book", systemImage: "plus") {
+                           showingAddScreen.toggle()
+                       }
+                   }
+               }
+               .sheet(isPresented: $showingAddScreen) {
+                   AddBookView()
+               }
+       }
+    }
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            modelContext.delete(book)
         }
-        .padding()
     }
 }
 
