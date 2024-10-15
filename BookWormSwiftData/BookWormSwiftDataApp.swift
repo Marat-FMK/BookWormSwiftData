b0e1b30fa4b177d3c3c5969ca25ad56ae72541c9
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-10-15 20:23:41 +0300

    Initial Commit

---
+++ BookWormSwiftData/BookWormSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormSwiftDataApp.swift
+//  BookWormSwiftData
+//
+//  Created by Marat Fakhrizhanov on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookWormSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
